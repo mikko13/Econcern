e241ba0d313c0560fd7baa34ddc86efc8a4c3166
RalphKennethDG <158722483+RalphKennethDG@users.noreply.github.com>
2024-04-09 14:25:05 +0800

    Add files via upload

---
+++ start.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles-start.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <title>ECOncern</title>
+</head>
+
+<body>
+    <div class="video-background">
+        <video autoplay muted loop>
+            <source src="start-vid.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </div>
+
+
+    <div class="container">
+        <h1><img src="header-title.png" alt="logo-title"></h1>
+        <button id="myButton" class="btn-primary">Login</button>
+        <button id="myButton" class="btn-primary">Sign up</button>
+        <button id="myButton" class="btn-primary">About us</button>
+    </div>
+
+</body>
+
+</html>
